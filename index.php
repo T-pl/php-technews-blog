8039947d40af84f4e92d6999d718d30da59a7b5c
T-pl <tiagofqu@gmail.com>
2023-01-05 09:25:35 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+
+<?php echo 'Hello World!';
+require_once 'sistema/config.php'; 
+include_once 'helpers.php'; //Funciona como se fosse um import;
+
+// require é uma forma de importar um arquivo essencial para o index;
+// require_once import um arquivo somente uma vez;
